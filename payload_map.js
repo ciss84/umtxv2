294247d0dce5e432bfdedb73c47a60eda8e3888e
Mugiwara <37186888+ciss84@users.noreply.github.com>
2025-03-30 15:48:51 +0200

    Add files via upload

--- payload_map.js
+++ payload_map.js
@@ -42,6 +42,16 @@ const payload_map = [
         binarySource: "",
         version: "1.0",
         customAction: CUSTOM_ACTION_APPCACHE_REMOVE
-    }
+    },
+    {
+        displayTitle: "websrv",
+        description: "Custom homebrew loader. Runs on port 8080.",
+        fileName: "websrv.elf",
+        author: "john-tornblom",
+        projectSource: "https://github.com/ps5-payload-dev/websrv",
+        binarySource: "https://github.com/ps5-payload-dev/websrv/releases/download/v0.22/Payload.zip",
+        version: "0.22",
+        toPort: 9021
+    }    
 
 ];
