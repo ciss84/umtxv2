a5208d0befb97dc48da22167fca8234b030990d7
Mugiwara <37186888+ciss84@users.noreply.github.com>
2025-12-07 23:53:32 +0100

    Add files via upload

--- payload_map.js
+++ payload_map.js
@@ -45,7 +45,7 @@ const payload_map = [
     },
     {
         displayTitle: "Install etaHEN to /data/",
-        description: "Download etaHEN.bin from host and install to /data/ (one-time setup for offline)",
+        description: "Download etaHEN.bin from host and install to /data/ (one-time setup for offline loading)",
         fileName: "",
         author: "LM LightningMods",
         projectSource: "https://github.com/ciss84/umtxv2",
@@ -64,6 +64,5 @@ const payload_map = [
         toPort: 9021
     }
 ];
-
 
 
