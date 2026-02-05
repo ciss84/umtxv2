2a5cf0b49744528749e75ec74eab0cce25850c7a
Mugiwara <37186888+ciss84@users.noreply.github.com>
2026-02-05 23:39:32 +0100

    Add files via upload

--- payload_map.js
+++ payload_map.js
@@ -13,7 +13,7 @@ const payload_map = [
     },  
     {
         displayTitle: "Install etaHEN to /data/",
-        description: "Download etaHEN.bin and install to /data/",
+        description: "Download etaHEN.bin from host and install to /data/ (one-time setup for offline loading)",
         fileName: "",
         author: "LM LightningMods",
         projectSource: "https://github.com/ciss84/umtxv2/25b",
@@ -23,7 +23,7 @@ const payload_map = [
     },   
     {
         displayTitle: "Install etaHEN to /data/",
-        description: "Download etaHEN.bin and install to /data/",
+        description: "Download etaHEN.bin from host and install to /data/ (one-time setup for offline loading)",
         fileName: "",
         author: "LM LightningMods",
         projectSource: "https://github.com/ciss84/umtxv2/24b",
@@ -31,36 +31,6 @@ const payload_map = [
         version: "2.4b",
         customAction: "ETAHEN_INSTALL"
     },
-    {
-        displayTitle: "Install Kstuff 1.6.7 to /data/etaHEN/",
-        description: "Download kstuff.elf and install to /data/etaHEN/",
-        fileName: "",
-        author: "EchoStretch",
-        projectSource: "https://github.com/ciss84/umtxv2/kstuff167",
-        binarySource: "https://github.com/EchoStretch",
-        version: "1.6.7",
-        customAction2: "KSTUFF_INSTALL"
-    },      
-    {
-        displayTitle: "Install Kstuff Lite to /data/etaHEN/",
-        description: "Download kstuff.elf and install to /data/etaHEN/",
-        fileName: "",
-        author: "EchoStretch",
-        projectSource: "https://github.com/ciss84/umtxv2/kstuff-lite",
-        binarySource: "https://github.com/EchoStretch",
-        version: "1.0",
-        customAction2: "KSTUFF_INSTALL"
-    },    
-    {
-        displayTitle: "Enable Save Data",
-        description: "np-fake-signin-ps5",
-        fileName: "np-fake-signin-ps5.elf",
-        author: "earthonion",
-        projectSource: "https://github.com/earthonion/np-fake-signin",
-        binarySource: "https://github.com/earthonion/np-fake-signin/releases/download/1.1/np-fake-signin-ps5.elf",
-        version: "1.1",
-        toPort: 9021
-    },   
     {
         displayTitle: "Browser appcache remover",
         description: "Deletes for only the current user in webkit-only mode",
