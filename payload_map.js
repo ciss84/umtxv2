778524b4db1ca12f9b33e7cbfe88b9d3e2e0fc0f
Mugiwara <37186888+ciss84@users.noreply.github.com>
2026-02-07 21:53:40 +0100

    Add files via upload

--- payload_map.js
+++ payload_map.js
@@ -32,6 +32,36 @@ const payload_map = [
         customAction: "ETAHEN_INSTALL"
     },
     {
+        displayTitle: "Install Kstuff 1.6.7 to /data/etaHEN/",
+        description: "Download kstuff.elf and install to /data/etaHEN/",
+        fileName: "",
+        author: "EchoStretch",
+        projectSource: "https://github.com/ciss84/umtxv2/kstuff167",
+        binarySource: "https://github.com/EchoStretch",
+        version: "1.6.7",
+        customAction2: "KSTUFF_INSTALL"
+    },      
+    {
+        displayTitle: "Install Kstuff Lite to /data/etaHEN/",
+        description: "Download kstuff.elf and install to /data/etaHEN/",
+        fileName: "",
+        author: "EchoStretch",
+        projectSource: "https://github.com/ciss84/umtxv2/kstuff-lite",
+        binarySource: "https://github.com/EchoStretch",
+        version: "1.0",
+        customAction2: "KSTUFF_INSTALL"
+    },    
+    {
+        displayTitle: "Enable Save Data",
+        description: "np-fake-signin-ps5",
+        fileName: "np-fake-signin-ps5.elf",
+        author: "earthonion",
+        projectSource: "https://github.com/earthonion/np-fake-signin",
+        binarySource: "https://github.com/earthonion/np-fake-signin/releases/download/1.1/np-fake-signin-ps5.elf",
+        version: "1.1",
+        toPort: 9021
+    },        
+    {
         displayTitle: "Browser appcache remover",
         description: "Deletes for only the current user in webkit-only mode",
         fileName: "",
